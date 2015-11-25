55488b08bae94c003e94f8c825f71d1353266b6d
Benjamin Hess <benjaminhess@comcast.net>
2015-11-25 11:08:30 -0500

    Restructures

--- tools/prep.py
+++ tools/prep.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#=================================================================
+# =================================================================
 # 1. Load the metadata from .xls files.
 # 2. Correct for ground sample distance variation.
 # 3. Remove vehicles with shadows.
@@ -17,27 +17,23 @@
 # 
 # crop size .......... dimensions in pixels          (int)
 # rotate ............. should the images be rotated? (bool)
-#=================================================================
+# =================================================================
+
 
 # Create a csv from the xls files.
 def load_xl(data_dir):
-    total = pd.DataFrame()
-    for i in range(20): 
-        fname = data_dir+'/DataSet_'+str(i+1)+'/DataSet'+str(i+1)+'.xls'
-        book = pd.read_excel(io=fname, sheetname=0, parse_cols=[1,2,3,7,8,9,15,28,47])
-        total = total.append(book)
+    dataframe = pd.DataFrame()
+    for i in range(20):
+        fname = data_dir + '/DataSet_' + str(i + 1) + '/DataSet' + str(i + 1) + '.xls'
+        book = pd.read_excel(io=fname, sheetname=0, parse_cols=[1, 2, 3, 7, 8, 9, 15, 28, 47])
+        dataframe = dataframe.append(book)
 
-    total = total[['Image Path', 'Image Name', 'Target Number', 
-                   'Intersection Polygon', 'Average Target Centroid',
-                   'Mode of Target Type', 'Average Target Orientation',
-                   'Average Target Shadow %', ' Average GSD']]
-    # im_sizes = total.iloc[:,7].unique()
-    # im_sizes.sort()
-    # print 'Image sizes:\n'
-    # for x in im_sizes:
-    #     print x
-    # total.to_csv(data_dir+'/datasets.csv')
-    
+    dataframe = dataframe[['Image Path', 'Image Name', 'Target Number',
+                           'Intersection Polygon', 'Average Target Centroid',
+                           'Mode of Target Type', 'Average Target Orientation',
+                           'Average Target Shadow %', ' Average GSD']]
+    return dataframe
+
 
 # rm -rf a folder
 def rmrf(folder):
@@ -52,17 +48,16 @@ def rmrf(folder):
 
 # Ground sample distance normalization
 # - Zooms in to match the minimum value
-def gsd(df):
+def gsdnorm(df):
     # Average GSD
-    val = np.unique(df.iloc[:,8].values)
+    val = np.unique(df.iloc[:, 8].values)
     base = val.min()
-    multiplier = gsd / base
     os.mkdir('/data/oirds/zoom')
-    for i, gsd in enumerate(df.iloc[:,8]):
-        i, gsd = 0, .1524
-        original = df.iloc[i,1][:-3]+'png'
-        infile = '/data/oirds/png/'+original
-        outfile = '/data/oirds/zoom/'+original
+    for i, gsd in enumerate(df.iloc[:, 8]):
+        multiplier = gsd / base
+        original = df.iloc[i, 1][:-3] + 'png'
+        infile = '/data/oirds/png/' + original
+        outfile = '/data/oirds/zoom/' + original
         im = Image.open(infile)
         x_size = int(im.size[0] * multiplier)
         y_size = int(im.size[1] * multiplier)
@@ -70,20 +65,22 @@ def gsd(df):
         im.save(outfile)
 
         # The polygon
-        matrix = df.iloc[i,3][1:-1].split(';')]
+        matrix = df.iloc[i, 3][1:-1].split(';')
         matrix = [x.split(' ') for x in matrix]
         matrix = pd.DataFrame(matrix)
         matrix = matrix.convert_objects(convert_numeric=True)
         matrix = matrix.multiply(multiplier).astype('int')
         matrix = matrix.to_string(header=False, index=False)
-        df.iloc[i,3] = re.sub(r' +', ' ', matrix).replace('\n ', ';')
+        df.iloc[i, 3] = re.sub(r' +', ' ', matrix).replace('\n ', ';')
 
         # The centroid
-        center = pd.to_numeric(df.iloc[1,4][1:-1].split())
-        df.iloc[1,4] *= multiplier
+        center = pd.to_numeric(df.iloc[1, 4][1:-1].split())
+        df.iloc[1, 4] = center * multiplier
+        return df
 
 
 def main():
+    global total, total
     import os
     from PIL import Image
     import pandas as pd
@@ -94,140 +91,141 @@ def main():
     else:
         from io import StringIO
 
+    data = '/data/oirds'
     try:
         chip_size = int(sys.argv[1])
-        half_chip = chip_size/2
+        half_chip = chip_size / 2
+
         # rmrf(data+'/no_car_crop')
-        os.mkdir(data+'/no_car_crop')
-
+        os.mkdir(data + '/no_car_crop')
         # rmrf(data+'/crop')
-        os.mkdir(data+'/crop')
+        os.mkdir(data + '/crop')
 
-        trainfile = data+'/train'+str(chip_size)+'.txt'
-        testfile = data+'/val'+str(chip_size)+'.txt'
+        crop = 1
 
     except ValueError:
         crop = 0
         chip_size = ''
 
-    data = '/data/oirds'
     # Extract data from the spreadsheets.
-    load_xl(data)
-    #total = pd.read_csv(data+'/datasets.csv', index_col=0)
+    total = load_xl(data)
+    # total = pd.read_csv(data+'/datasets.csv', index_col=0)
 
     # Correct for ground sample distance variation.
-    gsd(total)
+    total = gsdnorm(total)
 
     # Exclude vehicles with shadow values greater than or equal to 0.1.
     total = total[total['Average Target Shadow %'] < 0.1]
-    
+
     # Find the names of images with a second target.
-    multiples = total[total.iloc[:,2]==2].iloc[:,1]
+    multiples = total[total.iloc[:, 2] == 2].iloc[:, 1]
     # Limit the number of no-vehicle chips to the number of vehicle chips.
-    train_test_proportion = 5 # a 4 to 1 ratio
-    limit = len(total.iloc[:,2]) / train_test_proportion
+    train_test_proportion = 5  # a 4 to 1 ratio
+    limit = len(total.iloc[:, 2]) / train_test_proportion
     counter = 0
 
     rotate = sys.argv[2] in ['True', 'true', 'y', 'Y', 'Yes', 'yes']
 
+    trainfile = data + '/train' + str(chip_size) + '.txt'
+    testfile = data + '/val' + str(chip_size) + '.txt'
+
     with open(trainfile, 'w+') as train:
         with open(testfile, 'w+') as test:
             # Loop over the vehicles.
-            for i, ctr in enumerate(total.iloc[:,4]): # Centroid coordinates
-                original = total.iloc[i,1][:-3]+'png'
-                im1 = Image.open(data+'/png'+original)
-                uid = total.iloc[i,1][:-4]+str(total.iloc[i,2])+'_'+str(chip_size)
+            for i, ctr in enumerate(total.iloc[:, 4]):  # Centroid coordinates
+                original = total.iloc[i, 1][:-3] + 'png'
+                im1 = Image.open(data + '/png' + original)
+                uid = total.iloc[i, 1][:-4] + str(total.iloc[i, 2]) + '_' + str(chip_size)
 
                 # Rotate the image.
-                if rotate==True:
-                    copyfile(original, data+'/rotate'+original
-                    for i in range(15, 180, 15):
-                        img = im1.rotate(i)
-                        fname = data+'/rotate/'+uid+'_'+str(i)+'.png'
+                if rotate:
+                    copyfile(original, data + '/rotate' + original)
+                    for j in range(15, 180, 15):
+                        img = im1.rotate(j)
+                        fname = data + '/rotate/' + uid + '_' + str(j) + '.png'
                         img.save(fname)
-                        if i % train_test_proportion == 0:
-                            test.write(fname+' 1\n')
+                        if j % train_test_proportion == 0:
+                            test.write(fname + ' 1\n')
                         else:
-                            train.write(fname+' 1\n')
-                        
+                            train.write(fname + ' 1\n')
+
                 if crop != 0:
                     # Crop the image around the vehicles.
-                    txt = 'x y\n'+ctr.replace(']','').replace('[','')
+                    txt = 'x y\n' + ctr.replace(']', '').replace('[', '')
                     io_txt = StringIO(txt)
-                    ctr2 = pd.DataFrame.from_csv(io_txt, 
-                                                 sep=" ", 
-                                                 parse_dates=False, 
+                    ctr2 = pd.DataFrame.from_csv(io_txt,
+                                                 sep=" ",
+                                                 parse_dates=False,
                                                  index_col=None
                                                  ).apply(int)
 
                     w, h = im1.size
-                    ctr_x,ctr_y = ctr2.iloc[0],ctr2.iloc[1]
+                    ctr_x, ctr_y = ctr2.iloc[0], ctr2.iloc[1]
                     # The distance from the top-left corner to the 
                     #   left edge, top edge, right edge and bottom edge.
-                    l,u,r,low = ctr_x-half_chip,ctr_y-half_chip,ctr_x+half_chip,ctr_y+half_chip
+                    l, u, r, low = ctr_x - half_chip, ctr_y - half_chip, ctr_x + half_chip, ctr_y + half_chip
                     if ctr_x < half_chip:
-                        l,r = 0,chip_size
-                    
-                    if ctr_x > w-half_chip:
-                        l,r = w-chip_size,w
-                        
+                        l, r = 0, chip_size
+
+                    if ctr_x > w - half_chip:
+                        l, r = w - chip_size, w
+
                     if ctr_y < half_chip:
-                        u,low = 0,chip_size
-            
-                    if ctr_y > h-half_chip:
-                        u,low = h-chip_size,h
-                
-                    im2 = im1.crop((l,u,r,low))
-                    uid = total.iloc[i,1][:-4]+str(total.iloc[i,2])+'_'+str(chip_size)
-                    fname = data+'/crop/'+uid+'_0.png'
+                        u, low = 0, chip_size
+
+                    if ctr_y > h - half_chip:
+                        u, low = h - chip_size, h
+
+                    im2 = im1.crop((l, u, r, low))
+                    uid = total.iloc[i, 1][:-4] + str(total.iloc[i, 2]) + '_' + str(chip_size)
+                    fname = data + '/crop/' + uid + '_0.png'
                     im2.save(fname)
                     if i % train_test_proportion == 0:
-                        test.write(fname+' 1\n')
+                        test.write(fname + ' 1\n')
                     else:
-                        train.write(fname+' 1\n')
-                                    
+                        train.write(fname + ' 1\n')
 
                     # Tile the single-object images with "no car" chips.
-                    if total.iloc[i,1] not in multiples:
+                    if total.iloc[i, 1] not in multiples:
                         # Tile to the right.
                         # x
-                        for j in range((w-ctr_x-half_chip)/chip_size):
+                        for j in range((w - ctr_x - half_chip) / chip_size):
                             # y
-                            for k in range(h/chip_size):
-                                right = w - j*chip_size
-                                upper = k*chip_size
+                            for k in range(h / chip_size):
+                                right = w - j * chip_size
+                                upper = k * chip_size
                                 left = right - chip_size
                                 lower = upper + chip_size
                                 im3 = im1.crop((left, upper, right, lower))
-                                code = j*h/chip_size+k
-                                uid = total.iloc[i,1][:-4]+str(code)+'_'+str(chip_size)
-                                fname = data+'/no_car_crop/'+uid+'R.png'
+                                code = j * h / chip_size + k
+                                uid = total.iloc[i, 1][:-4] + str(code) + '_' + str(chip_size)
+                                fname = data + '/no_car_crop/' + uid + 'R.png'
                                 im3.save(fname)
                                 if counter < limit:
-                                    test.write(fname+' 0\n')
+                                    test.write(fname + ' 0\n')
                                 else:
-                                    train.write(fname+' 0\n')
+                                    train.write(fname + ' 0\n')
                                 counter += 1
                         # Tile to the left.
                         # x
-                        for m in range((ctr_x-half_chip)/chip_size):
+                        for m in range((ctr_x - half_chip) / chip_size):
                             # y
-                            for n in range(h/chip_size):
-                                left = m*chip_size
+                            for n in range(h / chip_size):
+                                left = m * chip_size
                                 right = left + chip_size
-                                upper = n*chip_size
+                                upper = n * chip_size
                                 lower = upper + chip_size
                                 im3 = im1.crop((left, upper, right, lower))
-                                code = m*h/chip_size+n
-                                uid = total.iloc[i,1][:-4]+str(code)+'_'+str(chip_size)
-                                fname = data+'/no_car_crop/'+uid+'L.png'
+                                code = m * h / chip_size + n
+                                uid = total.iloc[i, 1][:-4] + str(code) + '_' + str(chip_size)
+                                fname = data + '/no_car_crop/' + uid + 'L.png'
                                 im3.save(fname)
                                 if counter < limit:
-                                    test.write(fname+' 0\n')
+                                    test.write(fname + ' 0\n')
                                 else:
-                                    train.write(fname+' 0\n')
+                                    train.write(fname + ' 0\n')
                                 counter += 1
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
