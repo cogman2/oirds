b1d6ed93ddac4d5d023e1841f5120783ae6b6a11
erobertson <rwgdrummer@cnn-gpu-04.ec2.internal>
2015-11-30 22:59:13 +0000

    works up to selecting training/test set

--- tools/label_image.py
+++ tools/label_image.py
@@ -5,7 +5,10 @@
 
 #import h5py, os
 
+import lmdb
 label_colors = [(64,128,64),(192,0,128),(0,128,192),(0,128,64),(128,0,0)]
+modes = ['VEHICLE/CAR','VEHICLE/PICK-UP','VEHICLE/TRUCK','VEHICLE/UNKNOWN','VEHICLE/VAN']
+modeIndices = dict( zip( modes, [int(x) for x in range( len(modes) )] ) )
 
 def main():
     import os
@@ -14,7 +17,7 @@ def main():
     import itertools
     import glob
     import sys
-    import shapely
+    import random
     from PIL import Image
     if sys.version_info[0] < 3:
         from StringIO import StringIO
@@ -36,43 +39,66 @@ def main():
     imageNameIndex = 'Image Name'
     modeIndex = 'Mode of Target Type'
 
-# 1, 2, 3 = "Image Path", "Image Name", "Target Number"
-# 7, 8 = "Intersection Polygon", "Average Target Centroid"
-# 9, 15 = "Mode of Target Type", "Average Target Orientation"
+    randUniform = random.seed(23361)
 
-    cols = [1,2,3,7]
+# 1, 2, 3 = "Image Path", "Image Name", "Target Number"
+# 7, 9 = "Intersection Polygon", "Mode of Target Type"
+
+    cols = [1,2,3,7,9]
     xlsInfo = pd.DataFrame()
     for xlsFile in xlsFiles:
        xlsInfo = xlsInfo.append( pd.read_excel( io=xlsFile, parse_cols=cols, ignore_index=True ) )
 
+    xlsInfo['isTest'] = 0
+    xlsInfo = xlsInfo.reset_index()
+    out_db_train = lmdb.open('raw_train', map_size=int(94371840))
+    out_db_test = lmdb.open('raw_test', map_size=int(94371840))
+    label_db_train = lmdb.open('groundtruth_train', map_size=int(94371840))
+    label_db_test = lmdb.open('groundtruth_test', map_size=int(94371840))
+
     lastList=[]
     lastname=''
+
+    setIsTest(xlsInfo,0.18)
     for i,r in xlsInfo.iterrows():
-       if (lastname!= r[1] and len(lastList) > 0):
+       if (lastname!= r[2] and len(lastList) > 0):
            labelImage, rawImage = convertImg(lastname, lastList, parentDataDir)
-           labelImage.save(parentDataDir+"png/"+ lastname[0:lastname.index('.tif')] + ".png")
+           if (r[6]==1):
+              outGT(rawImage, out_db_test)
+              outGT(labelImage, label_db_test)
+           else:
+              outGT(rawImage, out_db_train)
+              outGT(labelImage, label_db_train)
+           #labelImage.save(parentDataDir+"/png/"+ lastname[0:lastname.index('.tif')] + ".png")
            # need to send to training or test set here!
            lastList=[]
        else:
            lastList.append(r)
-       lastname=r[1]
+       lastname=r[2]
 
+    out_db_train.close()
+    label_db_train.close()
+    out_db_test.close()
+    label_db_test.close()
     sys.exit(0)
 
-#    in_db_train = lmdb.open('train', map_size=int(94371840))
-#    out_db_train = lmdb.open('groundtruth_train', map_size=int(94371840))
+
 
 #with h5py.File('train.h5','w') as H:
  #   H.create_dataset( 'X', data=X ) # note the name X given to the dataset!
  #   H.create_dataset( 'y', data=y ) # note the name y given to the dataset!
 
-#    with in_db_train.begin(write=False) as in_txn_train:
- #    with out_db_train.begin(write=True) as out_txn_train:
- #       with open('train.txt', 'r') as train:
- #          for line in train:
- #             outGT(xlsInfo, line, idxNames, out_txn_train)
- #   out_db_train.close()
-#   in_db_train.close()
+def setIsTest(xlsInfo, percent):
+    import numpy as np
+    labelCounts = [0 for i in xrange(len(label_colors))]
+    for i,r in xlsInfo.iterrows():  
+       labelCounts[modeIndices[r[5]]] += 1
+    for i in xrange(len(labelCounts)):
+      labelCounts[i] = int(labelCounts[i]*percent)
+    for i in np.random.permutation(len(xlsInfo)):
+       if(labelCounts[modeIndices[xlsInfo.iloc[i,5]]] > 0):
+         xlsInfo.loc[i,'isTest']=1
+         labelCounts[modeIndices[xlsInfo.iloc[i,5]]] -= 1
 
 def convertImg(name,xlsInfoList, dir):
   from shapely.wkt import dumps, loads
@@ -80,16 +106,15 @@ def convertImg(name,xlsInfoList, dir):
   from shapely.geometry import polygon
   from PIL import Image
   print name + '-----------'
-  imRaw = Image.open(dir + '/png/' + xlsInfoList[0][1][0:xlsInfoList[0][1].index('.tif')] + '.png') 
+  imRaw = Image.open(dir + '/png/' + xlsInfoList[0][2][0:xlsInfoList[0][2].index('.tif')] + '.png') 
   imLabel = Image.new("RGB", imRaw.size, "white")
   for r in xlsInfoList:
-    poly = r[3].replace("[",'(').replace("]","").replace(";",",")
+    poly = r[4].replace("[",'(').replace("]","").replace(";",",")
     beg = poly[1:poly.index(',')]
     poly = 'POLYGON (' + poly + ',' + beg + '))'
-    print(poly)
     polyObj = loads(poly)
     try:
-        labelImage(imLabel, polyObj, label_colors[r[2]])
+        labelImage(imLabel, polyObj, label_colors[modeIndices[r[5]]])
     except:
         continue
   return imLabel, imRaw
@@ -103,15 +128,11 @@ def labelImage(img, poly, color):
      if poly.contains(Point(x, y)):
         img.putpixel((x, y), color)
 
-def outGT (xlsInfo, line, namesIdx, out_txn ):
-   imageFile = line.split()[0]
-   imageLabel = int(line.split()[1])
-   xlsRow = xlsInfo.iloc[names(os.path.basename(path)),:]
-   imRaw = Image.open(imageFile) # or load whatever ndarray you need
-   im = np.array(imRaw)
+def outGT (im, out_txn):
+   import caffe
+   im = np.array(im.resize(128,128))
    # convert to one dimensional ground truth labels
    tmp = np.uint8(np.zeros(im[:,:,0:1].shape))
-   tmp[:,:,0] = tmp[:,:,0] + np.prod(np.equal(im,label_colors[imageLabel]),2)
 
    # - in Channel x Height x Width order (switch from H x W x C)
    tmp = tmp.transpose((2,0,1))
