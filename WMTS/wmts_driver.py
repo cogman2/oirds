53a73e4db456bf3509e515873289cd7d45deb155
erobertson <robertson_eric@bah.com>
2016-03-30 15:21:23 -0400

    walker first test ok

--- WMTS/wmts_driver.py
+++ WMTS/wmts_driver.py
@@ -6,13 +6,15 @@ from urlparse import urlparse, urlunparse, parse_qs, ParseResult
 import numpy as np
 from PIL import Image
 from functools import partial
+import shape_walker
+import re
 
 
 def connectToGT(config):
   return WebMapService('http://129.206.228.72/cached/osm', version='1.1.1')
 
 def connectToDG(config):
-  return WebMapService("https://evwhs.digitalglobe.com/mapservice/wmsaccess?" + config['connectid'], username=config['uname'], password=config['passwd'], version='1.1.1')
+  return WebMapService("https://evwhs.digitalglobe.com/mapservice/wmsaccess?connectid=" + config['connectid'], username=config['uname'], password=config['passwd'], version='1.1.1')
 
 # pullGTImage(wmsConn, (-48.754156,-28.497557,-48.7509017,-28.494662), (256,256))
 def pullGTImage(wmsConn,dims, bbx):
@@ -29,11 +31,11 @@ def pullGTImage(wmsConn,dims, bbx):
   return bchannel, img
 
 def checkGTImage(img_array, dims):
-  return float(np.histogram(bchannel,2)[0][1])/float((dims[0]*dims[1])) >= 0.25
+  return float(np.histogram(img_array,2)[0][1])/float((dims[0]*dims[1])) >= 0.25
 
 # pullRawImage(wmsConn,'Aerial_CIR_Profile', (-48.754156,-28.497557,-48.7509017,-28.494662), (256,256))
 def pullRawImage(wmsConn,profile, bbx,dims):
-  return wms.getmap(layers=['DigitalGlobe:Imagery'],  srs='EPSG:4326',  bbox=bbx, size=dims,  format='image/png', transparent=True, BGCOLOR='0xFFFFFF', FEATUREPROFILE=profile)
+  return wmsConn.getmap(layers=['DigitalGlobe:Imagery'],  srs='EPSG:4326',  bbox=bbx, size=dims,  format='image/png', transparent=True, BGCOLOR='0xFFFFFF', FEATUREPROFILE=profile)
 
 def pullSaveGT(wmsConn, dims, bbx):
    indices, img = pullGTImage(wmsConn, dims, bbx)
@@ -41,16 +43,18 @@ def pullSaveGT(wmsConn, dims, bbx):
       fname = re.sub(r'[\[\]\(\) ]','',str(bbx).replace(',','_').replace('.','p').replace('-','n'))
       img.save(fname + '_gt.png')
       np.save(fname+'.npy',indices)
-      return true
-   return false
+      return True
+   return False
 
 def pullSaveRaw(wmsConn, profile, dims, bbx):
-   img = pullRawImage(wmsConn, profile, dims, bbx)
+   u = pullRawImage(wmsConn, profile, dims, bbx)
    fname = re.sub(r'[\[\]\(\) ]','',str(bbx).replace(',','_').replace('.','p').replace('-','n'))
-   img.save(fname + '_raw.png')
+   f = open(fname + "_raw.png","w")
+   f.write(u.read())
+   f.close()
 
 # eventually we will use this as the curried callback function
-def pullAndDoBoth(wmsConnGT, wmsConnRaw, profile, dims, bbx):
+def pullAndDoBoth(wmsConnRaw,wmsConnGT,profile, dims, bbx):
   if (pullSaveGT(wmsConnGT, dims, bbx)):
      pullSaveRaw(wmsConnRaw, profile, dims, bbx)
 
@@ -58,13 +62,20 @@ def stupidWalk(polys, func):
   for poly in polys:
      for point in poly.points:
         func((point[0]-0.03, point[1]-0.03, point[0]+0.03, point[1]+0.03))
+
+osm_meters_per_pixel = {20 : 0.1493, 19 : 0.2986, 18 : 0.5972, 17 : 1.1943, 16 : 2.3887 , 15 : 4.7773 , 14 : 9.5546 , 13 : 19.109, 12 : 38.219, 11 : 76.437, 10 : 152.87, 9  : 305.75, 8  : 611.50, 7  : 1222.99, 6  : 2445.98, 5  : 4891.97, 4  : 9783.94, 3  : 19567.88, 2  : 39135.76, 1  : 78271.52, 0 : 156412.0}
  
-def doit(polys):
-  wmsConn=connectToGT(dict())
+def shapeWalk(polys, dims, func):
+  for poly in polys:
+    shape_walker.shape_walk([x for x in poly.coords], 17, osm_meters_per_pixel, dims[0]/4, dims[0], func)
+
+def doit(polys,config):
+  dgwmsConn=connectToDG(config)
+  gtwmsConn=connectToGT(config)
   dims=(256,256)
   #currying ...for now, just use the GT, eventually we will use pullAndDoBoth
-  mycallback = partial(pullSaveGT,wmsConn, dims)
+  mycallback = partial(pullAndDoBoth,dgwmsConn,gtwmsConn, 'Aerial_CIR_Profile', dims)
   # to be replaced by smart walk...which will also need zoom level, starting point(possibily), initial direction and distance to move 
   # in pixels..probably put all this in a 'config' object which is a dictionary
-  stupidwalk(polys, mycallback)
+  shapeWalk(polys, dims, mycallback)
   
