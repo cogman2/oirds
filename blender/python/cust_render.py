95a76e7c880091abaa5ade5ef74217e167319b43
erobertson <robertson_eric@bah.com>
2016-05-12 13:25:28 -0400

    added new blender for truck with constraints on lighting

--- blender/python/cust_render.py
+++ blender/python/cust_render.py
@@ -2,24 +2,25 @@ import bpy
 import math
 import numpy as np
 import random
+import img_meta
+
+imp = img_meta.ImgMetaProcess()
+imp.openFile('img_desc.csv')
+
+imd = img.images['img1.png']
 
 # print all objects
 for obj in bpy.data.objects:
     print(obj.name)
 
-
 # print all scene names in a list
-print(bpy.data.scenes.keys())
+#print(bpy.data.scenes.keys())
 
 # change input image node
 tree = bpy.data.scenes['Scene'].node_tree
 imgNode=[n for n in tree.nodes if n.name=='Image'][0]
-imgNode.image.filepath='bg2.png'
-#print(bpy.data.images)
+imgNode.image.filepath=imd.name
 
-
-pi = 3.14159265
-fov = 75
 
 scene = bpy.data.scenes["Scene"]
 
@@ -27,49 +28,38 @@ scene = bpy.data.scenes["Scene"]
 lamp_object = bpy.data.objects['Lamp']
 point_object = bpy.data.objects['Point']
 car_object = bpy.data.objects['PickUp']
+camera_object = bpy.data.objects['Camera']
 
-off_nadir = 25.0
-sun_elev = 51.1304
-sun_azimuth = 41.2039
-truck_object_x_rot=90
 
-lz = 40
-camera_angle = 90
+camera_angle = camera_object.rotation_euler[2]
+lz = lamp_object.location[2]
+truck_object_z_rot=car_object.rotation_euler[2]
+
+off_nadir = imd.offnadir
+sun_azimuth = imd.azimuth
+sun_elev = imd.elevation
+placement = imd.getImagePlace()
+
 # 180 assumes the camera is aligned to the y axis
 lx = lz * math.cos(sun_elev) * math.cos(90 + camera_angle - sun_azimuth)
 ly = lz * math.cos(sun_elev) * math.sin(180 + camera_angle - sun_azimuth
 
 lamp_loc = np.array([lx,ly,lz])
 
-lamp_len = np.sqrt((lamp_loc*lamp_loc).sum())
-ax = -math.atan(lamp_loc[1]/lamp_loc[2])
-ay = math.acos(lamp_loc[2]/(lamp_loc_len*math.cos(ax)))
-az= math.atan(-math.cos(ax)*math.sin(ay)/math.sin(ax)) - math.atan(lamp_loc[0]/lamp_loc[1])
+truck_object_y_rot=placement[0]
+
+car_object.rotation_euler=(math.radians(truck_object_x_rot),math.radians(truck_object_y_rot),math.radians(truck_object_z_rot))
+
+# pixel to blender space conversion
+image_factor = 0.1
+car_object.location = (placement[1][0]*image_factor,placement[1][1]*image_factor,car_object.location[2])
 
 lamp_object.location = lamp_loc
 
-print(lamp_object.rotation_mode)
-print(lamp_object.rotation_euler)
-# can leave it in radians
-lamp_object.rotation_euler = (ax,ay,az)
-point_object.rotation_euler = (ax,ay,az)
-print(lamp_object.rotation_euler)
-
-print(car_object.rotation_euler)
-truck_object_y_rot=round(random.uniform(-180,180),4)
-# insert function here to calculate the y angle.
-truck_object_y_rot=0
-car_object.rotation_euler=(math.radians(truck_object_x_rot),math.radians(truck_object_y_rot),math.radians(truck_object_z_rot))
-print(car_object.rotation_euler)
-
-range=[7.5,7.5]
-# will use car dimesions and angle to restrict range
-#print(car_object.dimensions)
-
-print(car_object.location)
-car_object.location = (3.65382,-4.90261,0)
-print(car_object.location)
-
 # Render Scene and store the scene
 bpy.ops.render.render( write_still=True ,use_viewport=True)
+bpy.data.scenes["Scene"].render.image_settings.file_format = 'PNG'
+bpy.data.scenes["Scene"].render.filepath = '//plane'
 
+# Render Scene and store the scene 
+#bpy.ops.render.render( write_still=True ) 
