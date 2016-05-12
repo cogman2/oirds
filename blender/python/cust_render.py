10a19832baf029e01a82fd3b946329cdb63626b2
erobertson <robertson_eric@bah.com>
2016-05-12 18:59:18 -0400

    initial test for blender image is showing shadow and car is not visible

--- blender/python/cust_render.py
+++ blender/python/cust_render.py
@@ -7,7 +7,7 @@ import img_meta
 imp = img_meta.ImgMetaProcess()
 imp.openFile('img_desc.csv')
 
-imd = imp.images['img1.png']
+imd = imp.images['img1.png'][0]
 
 # print all objects
 for obj in bpy.data.objects:
@@ -33,7 +33,8 @@ camera_object = bpy.data.objects['Camera']
 
 camera_angle = camera_object.rotation_euler[2]
 lz = lamp_object.location[2]
-truck_object_z_rot=car_object.rotation_euler[2]
+truck_object_y_rot=car_object.rotation_euler[1]
+truck_object_x_rot=car_object.rotation_euler[0]
 
 off_nadir = imd.offnadir
 sun_azimuth = imd.azimuth
@@ -46,20 +47,20 @@ ly = lz * math.cos(sun_elev) * math.sin(180 + camera_angle - sun_azimuth)
 
 lamp_loc = np.array([lx,ly,lz])
 
-truck_object_y_rot=placement[0]
+truck_object_z_rot=placement[0]
 
 car_object.rotation_euler=(math.radians(truck_object_x_rot),math.radians(truck_object_y_rot),math.radians(truck_object_z_rot))
 
 # pixel to blender space conversion
-image_factor = 0.1
+image_factor = 0.03
 car_object.location = (placement[1][0]*image_factor,placement[1][1]*image_factor,car_object.location[2])
 
 lamp_object.location = lamp_loc
 
 # Render Scene and store the scene
-bpy.ops.render.render( write_still=True ,use_viewport=True)
 bpy.data.scenes["Scene"].render.image_settings.file_format = 'PNG'
-bpy.data.scenes["Scene"].render.filepath = '//plane'
+bpy.data.scenes["Scene"].render.filepath = 'newimg'
+bpy.ops.render.render( write_still=True ,use_viewport=True)
 
 # Render Scene and store the scene 
 #bpy.ops.render.render( write_still=True ) 
