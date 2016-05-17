9795ce18ebd3006dad13928435f9ed569147d796
erobertson <robertson_eric@bah.com>
2016-05-17 15:10:59 -0400

    Now creates image mask and uses delta transforms for rotation and scale.  Furthermore, updated doc

--- blender/python/cust_render.py
+++ blender/python/cust_render.py
@@ -26,13 +26,30 @@ for i in range(iterations):
         placement = imd.getImagePlace()
 
         car_object = bpy.data.objects[config['obj.car_name']] 
+        mask_lamp_object = bpy.data.objects[config['obj.mask_lamp_name']]
+        lamp_object = bpy.data.objects[config['obj.lamp_name']]
 
         # 180 and 90 assumes the camera is aligned to the y axis
         sc.move_lamp(sun_elev, sun_azimuth, 30)
         sc.move_car(placement[1][0]*image_factor,placement[1][1]*image_factor,car_object.location[2]+0.05,math.radians(placement[0]))
         sc.scale_car(imd.zoom_factor)
 
+        # mask is already constrained to point at the car or straight down, so just move the location (do not change 'z' axis)
+        mask_lamp_object.location = (car_object.location[0],car_object.location[1],mask_lamp_object.location[2])
+
         # Render Scene and store the scene
+        mask_lamp_object.hide_render = True
+        lamp_object.hide_render = False
         bpy.data.scenes[config['obj.scene_name']].render.image_settings.file_format = 'PNG'
-        bpy.data.scenes[config['obj.scene_name']].render.filepath = 'o_' +  imageName[0:imageName.find('.')] + '_' + str(count)
+        bpy.context.scene.node_tree.nodes['MaskOutput'].base_path = 'trash'
+        bpy.context.scene.node_tree.nodes['MaskOutput'].file_slots[0].path = 'mask_' + str(count) + '.png'
+        bpy.data.scenes[config['obj.scene_name']].render.filepath = 'images/o_' +  imageName[0:imageName.find('.')] + '_' + str(count)
+        bpy.ops.render.render( write_still=True ,use_viewport=True)
+
+        # Render Scene with mask
+        mask_lamp_object.hide_render = False
+        lamp_object.hide_render = True
+        bpy.context.scene.node_tree.nodes['MaskOutput'].base_path = 'masks'
+        bpy.context.scene.node_tree.nodes['MaskOutput'].file_slots[0].path = 'mask_' + str(count) + '.png'
+        bpy.data.scenes[config['obj.scene_name']].render.filepath = 'trash/o_' +  imageName[0:imageName.find('.')] + '_' + str(count)
         bpy.ops.render.render( write_still=True ,use_viewport=True)
