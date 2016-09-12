2d432a189ba72e3c40e88d6c1c43a87abb60d498
Cloud User <ec2-user@ip-172-31-52-167.ec2.internal>
2016-09-12 12:17:53 -0400

    some changes to fixDocker etc.

--- docker/coco/cocoinstall.sh
+++ docker/coco/cocoinstall.sh
@@ -2,8 +2,8 @@
 # Shell script to install COCO2014
 #
 # Alter to your desired directory
-MY_DIRECTORY="/opt/py-faster-rcnn/data"
-MY_API_DIRECTORY="/opt/py-faster-rcnn/data/coco"
+MY_DIRECTORY="/opt/py-faster-rcnn/data/coco"
+MY_API_DIRECTORY="/opt/py-faster-rcnn/data"
 if [ ! -d $MY_DIRECTORY ]; then
     mkdir $MY_DIRECTORY
 fi
@@ -14,19 +14,27 @@ ls *.zip | xargs -i rm {}
 # Get image and annotation files
 wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
 wget http://msvocds.blob.core.windows.net/coco2014/val2014.zip
+wget http://msvocds.blob.core.windows.net/coco2014/test2014.zip
 wget http://msvocds.blob.core.windows.net/annotations-1-0-3/instances_train-val2014.zip
 wget http://msvocds.blob.core.windows.net/annotations-1-0-3/person_keypoints_trainval2014.zip
 wget http://msvocds.blob.core.windows.net/annotations-1-0-4/image_info_test2014.zip
-ls *.tar | xargs -i tar xf {} 
+wget https://data.vision.ee.ethz.ch/jpont/mcg/SCG-COCO-train2014-proposals.tgz
+
+FILE=selective_search_data.tgz
+wget http://www.cs.berkeley.edu/~rbg/fast-rcnn-data/$FILE
 ls *.zip | xargs -i unzip {} 
 tar -xzf selective_search_data.tgz 
+python $MY_DIRECTORY/../lib/datasets/tools/mcg_munge.py /SCG-COCO-train2014-proposals
+mkdir images
+mv train_2014 test_2014 val_2014 images
 if [ ! -d $MY_API_DIRECTORY ]; then
     mkdir $MY_API_DIRECTORY
 fi
 cd  $MY_API_DIRECTORY
-# Install COCO API
 wget https://github.com/pdollar/coco/archive/master.zip
 unzip master.zip
+# Install COCO API
+# Dependencies must already be installed, e.g., Cython
 cd coco-master/PythonAPI
 make
 make install
--- fixDockerNet.sh
+++ fixDockerNet.sh
@@ -8,3 +8,4 @@ service docker start
  docker rm -v $( docker ps -a -q -f status=exited)
  docker rmi $( docker images -f "dangling=true" -q)
  docker run -v $(which docker):/bin/docker -v /var/run/docker.sock:/var/run/docker.sock -v $(readlink -f /var/lib/docker):/var/lib/docker --rm martin/docker-cleanup-volumes
+#dockerd --storage-opt dm.basesize=20G
